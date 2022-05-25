7f928247520942016c1cb802699c85982cda168f
Ermolay <es.ermolaev2005@gmail.com>
2022-05-25 17:52:59 +0300

    goimager require

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/egor-erm/goisgen
 
 go 1.18
+
+require github.com/egor-erm/goimager v0.0.0-20220520185828-737dace64a69
+
+require (
+	github.com/go-gl/mathgl v1.0.0 // indirect
+	golang.org/x/image v0.0.0-20190321063152-3fc05d484e9f // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package goisgen
 
-import "fmt"
+import (
+	"fmt"
+
+	goimage "github.com/egor-erm/goimager/imager"
+)
 
 func main() {
 	fmt.Println("ky")
+	img := goimage.New("eg.png", 100, 100)
+	img.FillAllHex("#66FF00")
+	img.DrowHexRect(10, 10, 89, 89, "#00FFD5")
+	img.Save()
 }
